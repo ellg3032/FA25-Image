355c45f61379ae0a9c8c84d6a630fed68757df13
Elliot Goe <egoe3032@gmail.com>
2025-11-19 09:59:47 -0700

    in progress proj 3 rough draft

--- aframe-example-code-main/ImageLab11/script.js
+++ aframe-example-code-main/ImageLab11/script.js
@@ -1,15 +1,34 @@
-console.log("running");
-const pokeball = document.getElementById("pkball1");
+const pokeball = document.getElementById("pkball2");
+const pokeball2 = document.getElementById("pkball3");
+const pokeball3 = document.getElementById("pkball1");
 
 pokeball.addEventListener("click", charmander);
+pokeball2.addEventListener("click", squirtle);
+pokeball3.addEventListener("click", bulbasaur);
 
 function charmander() {
-    console.log("pressed");
     pokeball.setAttribute("src", "#charmander");
     pokeball.setAttribute("color", "#FFA500");
     //pokeball.removeAttribute("mtl");
     pokeball.setAttribute("rotation", "-90 0 0");
-    pokeball.setAttribute("scale", ".02 .02 .02");
-    pokeball.setAttribute("position", "-.2 0 .5");
+    pokeball.setAttribute("scale", ".03 .03 .03");
+    pokeball.setAttribute("position", "-3.8 -.6 .2");
     pokeball.setAttribute("mtl", "null");
+}
+
+function squirtle() {
+    pokeball2.setAttribute("src", "#squirtle");
+    pokeball2.setAttribute("color", "#FFA500");
+    //pokeball.removeAttribute("mtl");
+    pokeball2.setAttribute("scale", ".0012 .0012 .0012");
+    pokeball2.setAttribute("position", "-2 -.6 .2");
+    pokeball2.setAttribute("mtl", "null");
+}
+
+function bulbasaur() {
+    pokeball3.setAttribute("src", "#bulbasaur");
+    pokeball3.setAttribute("color", "#FFA500");
+    //pokeball.removeAttribute("mtl");
+    pokeball3.setAttribute("scale", ".0015 .0015 .0015");
+    pokeball3.setAttribute("position", "-0.2 -.6 .2");
 }
